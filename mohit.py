5aedc886a2eab428c1524f729f0fce0281f287c8
mohit <mohit.negi@cloudeq.com>
2022-05-12 19:40:05 +0530

    my repo

--- mohit.py
+++ mohit.py
@@ -1,7 +1,8 @@
 # Python program to check if the input number is odd or even.
 # A number is even if division by 2 gives a remainder of 0.
 # If the remainder is 1, it is an odd number.
-
+import sys
+num1=sys.argv[7]
 num = int(input("Enter a number: "))
 if (num % 2) == 0:
    print("{0} is Even".format(num))
